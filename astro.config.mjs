fca5e3d5b38a26ce61dd8bb9799929067d4a0bd1
Brett Egbert <brett@crossinguard.dev>
2025-11-22 15:52:06 -0800

    add custom css, logo, and favicon

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,26 +1,44 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'My Docs',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
-	],
+  integrations: [
+    starlight({
+      title: "crossinguardocs",
+      logo: {
+        src: "./src/assets/crystal-circle-logo.svg",
+      },
+      social: [
+        {
+          icon: "email",
+          label: "Email",
+          href: "mailto:brett@crossinguard.dev",
+        },
+        {
+          icon: "github",
+          label: "GitHub",
+          href: "https://github.com/crossinguard",
+        },
+      ],
+      customCss: [
+        // Relative path to your custom CSS file
+        "./src/styles/custom.css",
+      ],
+      sidebar: [
+        {
+          label: "Guides",
+          items: [
+            // Each item here is one entry in the navigation menu.
+            { label: "Example Guide", slug: "guides/example" },
+          ],
+        },
+        {
+          label: "Reference",
+          autogenerate: { directory: "reference" },
+        },
+      ],
+    }),
+  ],
 });
